230a6d2f3fa3ab93f3f2b41721803c3964981c14
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2024-10-04 20:34:54 +0100

    Create Schema

---
+++ code/jvm/src/main/kotlin/pt/isel/daw/sql/create-schema.sql
@@ -0,0 +1,62 @@
+create schema dbo;
+
+create table dbo.Users(
+    id int generated always as identity primary key,
+    username VARCHAR(64) unique not null,
+    email VARCHAR(64) unique not null,
+    password_validation VARCHAR(256) not null
+);
+
+create table dbo.Tokens(
+    token_validation VARCHAR(256) primary key,
+    user_id int references dbo.Users(id),
+    created_at bigint not null,
+    last_used_at bigint not null
+);
+
+create table dbo.Invitation_Register(
+    id serial not null primary key,
+    user_id int references dbo.Users(id),
+    expired BOOLEAN not null
+);
+
+create table dbo.Channels(
+    id serial not null unique primary key,
+    name VARCHAR(64) unique not null,
+    owner_id int references dbo.Users(id),
+    rules VARCHAR(64) not null
+);
+
+create table dbo.Public_Channels(
+    channel_id serial unique references dbo.Channels(id)
+);
+
+create table dbo.Join_Public_Channels(
+    user_id int references dbo.Users(id),
+    public_ch_id serial references dbo.Public_Channels(channel_id)
+);
+
+create table dbo.Private_Channels(
+    channel_id serial unique references dbo.Channels(id)
+);
+
+create table dbo.Invitation_Channels(
+    id serial not null unique primary key,
+    owner_id int references dbo.Users(id),
+    privacy int not null,
+    expired BOOLEAN not null
+);
+
+create table dbo.Invite_Private_Channels(
+    user_id int references dbo.Users(id),
+    private_ch serial references dbo.Private_Channels(channel_id),
+    invite_id serial references dbo.Invitation_Channels(id)
+);
+
+create table dbo.Messages(
+    id serial not null unique primary key,
+    channel_id serial references dbo.Channels(id),
+    user_id int references dbo.Users(id),
+    text VARCHAR(64) not null,
+    timestamp bigint not null
+);
