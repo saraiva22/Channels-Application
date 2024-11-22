6382ae2eb0c2043cda58a8d0fda56dea7c2c756b
Francisco Saraiva <a49462@alunos.isel.pt>
2024-11-22 16:14:35 +0000

    create the project structure - Frontend

---
+++ code/js/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>DAW</h1>
+      <h3>Group 10</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ code/js/webpack.dev.mjs
@@ -0,0 +1,21 @@
+import { server } from 'typescript';
+
+export default {
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
