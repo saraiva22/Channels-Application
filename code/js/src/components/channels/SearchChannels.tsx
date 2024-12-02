517d0a754d94fa4b58cb26bf544a373283d6baf4
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2024-12-02 13:21:03 +0000

    Create SearchChannels.tsx

---
+++ code/js/src/components/channels/SearchChannels.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// parecido com create Channel
+//
+export function SearchChannels() {
+    return (
+        <div>
+            <h1>Search For New Channels</h1>
+            <input></input>
+        </div>
+    )
+}
